455b3b7959015f5799ec0d754571ff56488ea9f6
amlan <amlan@compass939>
2021-06-29 21:08:42 +0530

    Upload first

--- Selenium/src/SeleniumTest.java
+++ Selenium/src/SeleniumTest.java
@@ -18,7 +18,7 @@ public class SeleniumTest
 	    driver.get("https://www.amazon.in/Siwi-Headphones-Samsung-Galaxy-M30/dp/B08XMN8M4M/ref=sr_1_1_sspa?dchild=1&keywords=earphones&qid=1624976271&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzODlRQTNYN0U1MUpMJmVuY3J5cHRlZElkPUEwMTcxMjM0N0I5VEEyR05OMDlTJmVuY3J5cHRlZEFkSWQ9QTAwMDY3MzYxRkkzMkw4RE9FNlJLJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==");
 		driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
 		driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div/div[1]/div[3]/div/a[4]/div[1]/span[2]")).click();
-		driver.findElement(By.id("a-autoid-0-announce")).click();
+		
 		
 	}
 	
